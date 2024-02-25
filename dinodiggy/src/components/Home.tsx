a8a0e172d78a1020b54a09550e332c92dee24c86
Goose9999 <snakeboot19@gmail.com>
2024-02-25 01:31:29 -0600

    home page change

--- dinodiggy/src/components/Home.tsx
+++ dinodiggy/src/components/Home.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-        
+            <div className='container'>
+            <div className='resource-container'>
+                <b>Welcome to our RowdyHacks 2024 Web App !</b>
+                <p>
+                Unearth hidden treasures! Our app harnesses climate and geolocation data to uncover nearby fossils.
+                </p>
+            </div>
+            </div>
         </div>
     );
 };
